9f97a8ee57f57d863f7f1ead0c4af36cd0a3c34a
dhconverter <91861315+dhconverter@users.noreply.github.com>
2025-02-08 22:43:58 -0800

    latest commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Slovak Learner 
+
+Learn Slovak via Fetch API!
+
+## Attribution
+
+Slovak to English Dictionary from SMenigat's [1000 Most Common Words](https://github.com/SMenigat/thousand-most-common-words/tree/master)
+
+Using sparkle vectors and concepts borrowed from [Simey's Pokemon-Holo Cards](https://github.com/simeydotme/pokemon-cards-css)
+
+
+Shiny-on-hover effect from [this tutorial](https://bholmes.dev/blog/a-shiny-on-hover-effect-that-follows-your-mouse-css/) by BHolmes.
--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 async function fetchData() {
-    let randomWord = Math.floor(Math.random() * 1001);
+    
 
     try {
-        const enWord = document.getElementById("en-word");
-        const szWord = document.getElementById("sz-word");
+        const enWord = document.querySelectorAll('#en-word');
+        const szWord = document.querySelectorAll('#sz-word');
         const response = await fetch("/sk.json");    
         if(!response.ok){
             throw new Error("Error fetching dictionary file.");
         }
 
         const data = await response.json();
-        const dictionaryData = data.words[randomWord];
-        enWord.innerText = dictionaryData.englishWord;
-        szWord.innerText = dictionaryData.targetWord;
+        // LENGTH of enWord/szWord attributes appearing in HTML. by default, 4
+        const length = Math.min(enWord.length, szWord.length);
+        for (let i = 0; i < length; i++) {
+            const dictionaryData = data.words[randomWord()];
+            enWord[i].innerText = dictionaryData.englishWord;
+            szWord[i].innerText = dictionaryData.targetWord;
+        };
     } 
     catch(error) {
         console.error(error)
     }
 }
 
+function randomWord() {
+    let randomWord = Math.floor(Math.random() * 1001);
+    return randomWord;
+}
+
 fetchData();
+
+// create a new flashcard; max of 8, default of four
+function createFlashcard(){
+
+}
+
+const rareCard = document.querySelector('.rare');
+rareCard.addEventListener('mousemove', (e) => {
+    const { x, y } = rareCard.getBoundingClientRect();
+    rareCard.style.setProperty("--x", e.clientX - x);
+    rareCard.style.setProperty("--y", e.clientY - y);
+})
